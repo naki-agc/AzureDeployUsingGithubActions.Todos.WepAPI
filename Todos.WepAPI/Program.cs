7a2e4878c75724493e05d5fcd736bdc2593bb03b
naki-agc <nakiagca10@gmail.com>
2025-09-09 18:54:16 +0300

    Add project files.

---
+++ Todos.WepAPI/Program.cs
@@ -0,0 +1,16 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
+
+var app = builder.Build()
+    ;
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapGet("/", () => "Hello World!");
+app.MapGet("/getAll", () =>Results.Ok( new List<string>() 
+{ "1", "2"
+}));
+
+app.Run();
